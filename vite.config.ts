52a99ff64abc33870d30f22e8a6385685c7b9143
Otsuka Masato <ootsukamase@gmail.com>
2024-05-12 23:33:08 +0900

    github-pages用の設定の作成

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,26 @@ import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
+
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix(), tsconfigPaths()],
+  base: "/make-3d-printable-yoyo/",
+  plugins: [
+    remix({
+      ssr: false,
+      basename: "/make-3d-printable-yoyo/",
+      buildEnd: (args) => {
+        if (!args.viteConfig.isProduction) return;
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html") // 全てが index.html に行かない場合があるので、念の為
+        );
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
