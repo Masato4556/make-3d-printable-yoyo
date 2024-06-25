843716593f5f6c3545a16f47cb88f6444bfa7a40
Otsuka Masato <ootsukamase@gmail.com>
2024-06-25 23:27:17 +0900

    coreのy座標を正の値で指定できるように変更

--- app/hooks/use-yoyo-geometry.ts
+++ app/hooks/use-yoyo-geometry.ts
@@ -16,59 +16,61 @@ const BEARING_TYPES: Record<string, BearingType> = {
   sizeC: "sizeC",
 } as const;
 
+// coreのパスを作成
+const CORE_PATH: Record<BearingType, Vector2[]> = {
+  sizeC: [new Vector2()].concat(
+    new Vector2(0, 1), // 底部の開始点
+    new Vector2(2, 1),
+    new Vector2(2, 5),
+    new CubicBezierCurve(
+      new Vector2(2.5, 5), // 開始点
+      new Vector2(3.15, 5), // コントロールポイント1
+      new Vector2(3.15, 5), // コントロールポイント2
+      new Vector2(3.15, 4.5) // 終点
+    ).getPoints(8),
+    new Vector2(3.15, 3),
+    new CubicBezierCurve(
+      new Vector2(3.85, 3), // 開始点
+      new Vector2(4.15, 3), // コントロールポイント1
+      new Vector2(4.15, 3), // コントロールポイント2
+      new Vector2(4.15, 2.7) // 終点
+    ).getPoints(8),
+    new Vector2(4.15, 1.59),
+    new Vector2(6.45, 1.59),
+    new CubicBezierCurve(
+      new Vector2(6.45, 3.14 - 0.2), // 開始点
+      new Vector2(6.45, 3.14), // コントロールポイント1
+      new Vector2(6.45, 3.14), // コントロールポイント2
+      new Vector2(6.45 - 0.2, 3.14) // 終点
+    ).getPoints(8),
+    new CubicBezierCurve(
+      new Vector2(7.1 - 0.2, 3.14), // 開始点
+      new Vector2(7.1, 3.14), // コントロールポイント1
+      new Vector2(7.1, 3.14), // コントロールポイント2
+      new Vector2(7.1, 3.14 - 0.2) // 終点
+    ).getPoints(8),
+    new Vector2(7.1, 1.885),
+    new Vector2(9.55, 1.885),
+    new CubicBezierCurve(
+      new Vector2(9.55, 3.085 - 0.3), // 開始点
+      new Vector2(9.55, 3.085), // コントロールポイント1
+      new Vector2(9.55, 3.085), // コントロールポイント2
+      new Vector2(9.55 - 0.3, 3.085) // 終点
+    ).getPoints(8),
+    new Vector2(10.55, 3.085)
+  ),
+} as const;
+
 const useYoyoCorePath = function (bearingType: BearingType) {
   const corePath: Vector2[] = useMemo(() => {
     // coreのパスを作成
     // 底面のy座標を0とし、ベアリング受けの部位をy軸負方向に伸ばしている
     // FIXME: y軸負方向に伸ばしているのは分かりにくいので、正方向に伸ばすように修正したい
-    switch (
-      bearingType // FIXME: switch文を使わずにオブジェクトのプロパティを参照するように修正する
-    ) {
-      case BEARING_TYPES.sizeC:
-        return [new Vector2()].concat(
-          new Vector2(0, -1), // 底部の開始点
-          new Vector2(2, -1),
-          new Vector2(2, -5),
-          new CubicBezierCurve(
-            new Vector2(2.5, -5), // 開始点
-            new Vector2(3.15, -5), // コントロールポイント1
-            new Vector2(3.15, -5), // コントロールポイント2
-            new Vector2(3.15, -4.5) // 終点
-          ).getPoints(8),
-          new Vector2(3.15, -3),
-          new CubicBezierCurve(
-            new Vector2(3.85, -3), // 開始点
-            new Vector2(4.15, -3), // コントロールポイント1
-            new Vector2(4.15, -3), // コントロールポイント2
-            new Vector2(4.15, -2.7) // 終点
-          ).getPoints(8),
-          new Vector2(4.15, -1.59),
-          new Vector2(6.45, -1.59),
-          new CubicBezierCurve(
-            new Vector2(6.45, -(3.14 - 0.2)), // 開始点
-            new Vector2(6.45, -3.14), // コントロールポイント1
-            new Vector2(6.45, -3.14), // コントロールポイント2
-            new Vector2(6.45 - 0.2, -3.14) // 終点
-          ).getPoints(8),
-          new CubicBezierCurve(
-            new Vector2(7.1 - 0.2, -3.14), // 開始点
-            new Vector2(7.1, -3.14), // コントロールポイント1
-            new Vector2(7.1, -3.14), // コントロールポイント2
-            new Vector2(7.1, -(3.14 - 0.2)) // 終点
-          ).getPoints(8),
-          new Vector2(7.1, -1.885),
-          new Vector2(9.55, -1.885),
-          new CubicBezierCurve(
-            new Vector2(9.55, -(3.085 - 0.3)), // 開始点
-            new Vector2(9.55, -3.085), // コントロールポイント1
-            new Vector2(9.55, -3.085), // コントロールポイント2
-            new Vector2(9.55 - 0.3, -3.085) // 終点
-          ).getPoints(8),
-          new Vector2(10.55, -3.085)
-        );
-      default:
-        return [];
+    if (!CORE_PATH[bearingType]) {
+      throw new Error(`Invalid bearing type: ${bearingType}`);
     }
+
+    return CORE_PATH[bearingType];
   }, [bearingType]);
   const [coreWidth, coreHeight] = corePath.reduce(
     (acc, cur) => {
@@ -76,7 +78,12 @@ const useYoyoCorePath = function (bearingType: BearingType) {
     },
     [0, 0]
   );
-  return { corePath, coreWidth, coreHeight };
+
+  return {
+    corePath: corePath.map((v) => new Vector2(v.x, -v.y)), // y座標0をcoreとwingの接合部にするため、coreをy座標負の方向に伸ばす
+    coreWidth,
+    coreHeight,
+  };
 };
 
 export const useYoyoGeometry = function (props: Props) {
