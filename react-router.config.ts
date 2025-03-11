7917b3148678be10383f498c689b6cdad925a4fd
Otsuka Masato <ootsukamase@gmail.com>
2025-03-11 21:29:16 +0900

    誤ってSSRになっていたので修正

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,6 +1,6 @@
 import type { Config } from "@react-router/dev/config";
 import { vercelPreset } from "@vercel/react-router/vite";
 export default {
-  ssr: true,
+  ssr: false,
   presets: [vercelPreset()],
 } satisfies Config;
