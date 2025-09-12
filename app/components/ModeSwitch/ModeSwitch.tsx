2aa4a288e8ab5515eaa3bc4f4a630603f52c0b40
大塚正斗 <ootsukamase@gmail.com>
2025-09-12 23:24:46 +0900

    ボタン色の修正

--- app/components/ModeSwitch/ModeSwitch.tsx
+++ app/components/ModeSwitch/ModeSwitch.tsx
@@ -8,17 +8,19 @@ import classes from "./style.module.scss";
 
 export function ModeSwitch() {
   const { mode, change } = useModeStore();
+
+  // もう少しプログレス感、進行している感（EDITの次がPREVIEWですよ！っていう感じ）が欲しい
   return (
     <div className={classes["ui-header"]}>
       <ModeSwitchButton
-        label={"SHAPE"}
+        label={"EDIT"}
         onClick={() => {
           change(Mode.PATH);
         }}
         isActive={mode === Mode.PATH}
       />
       <ModeSwitchButton
-        label={"DOWNLOAD"}
+        label={"PREVIEW"}
         onClick={() => {
           change(Mode.MODEL);
         }}
