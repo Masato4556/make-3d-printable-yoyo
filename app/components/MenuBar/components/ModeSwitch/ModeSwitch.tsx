70d3ac03f49442de16d57e4560950e8db7fa2e22
大塚正斗 <ootsukamase@gmail.com>
2025-09-26 21:26:12 +0900

    パス編集画面をメイン画面として、プレビュー画面のオンオフを切り替えられるように変更

--- app/components/MenuBar/components/ModeSwitch/ModeSwitch.tsx
+++ app/components/MenuBar/components/ModeSwitch/ModeSwitch.tsx
@@ -9,21 +9,19 @@ import classes from "./style.module.scss";
 export function ModeSwitch() {
   const { mode, change } = useModeStore();
 
+  const toggleMode = () => {
+    const nextMode = mode === Mode.MODEL ? Mode.PATH : Mode.MODEL;
+    change(nextMode);
+  };
+
+  const label = mode === Mode.MODEL ? "PREVIEW: ON" : "PREVIEW: OFF";
+
   // もう少しプログレス感、進行している感（EDITの次がPREVIEWですよ！っていう感じ）が欲しい
   return (
     <div className={classes["ui-header"]}>
       <ModeSwitchButton
-        label={"EDIT"}
-        onClick={() => {
-          change(Mode.PATH);
-        }}
-        isActive={mode === Mode.PATH}
-      />
-      <ModeSwitchButton
-        label={"PREVIEW"}
-        onClick={() => {
-          change(Mode.MODEL);
-        }}
+        label={label}
+        onClick={toggleMode}
         isActive={mode === Mode.MODEL}
       />
     </div>
